24a8bc9202daf29705b9da5966e9b2479dcd2189
JonasKiwia <jonas.kiwia@student.moringaschool.com>
2025-06-20 04:17:44 +0300

    Worked on the Timer and the Track

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+// using UnityEngine.UI;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
@@ -6,13 +8,24 @@ public class Timer : MonoBehaviour
     private float elapsedTime;
     private bool hasStartedLap = false;
 
+    // public GameObject lapText;
+    public TMP_Text lapText;
+
     void Update()
     {
-        if (hasStartedLap)
-        {
-            elapsedTime = Time.time - startTime;
-            Debug.Log(elapsedTime);
-        }
+        // if (hasStartedLap)
+        // {
+        //     elapsedTime = Time.time - startTime;
+        //     // Debug.Log(elapsedTime);
+
+        //     lapText.GetComponent<Text>().text = "Lap Time:" + elapsedTime.ToString();
+        // }
+
+        if (!hasStartedLap || lapText == null)
+            return;
+        
+        elapsedTime = Time.time - startTime;
+        lapText.text = "Lap Time: " + elapsedTime.ToString("F2");
     }
 
     private void OnTriggerEnter(Collider other)
